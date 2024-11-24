d3adc1fbf7d452588222ddf9e2be9f73730ca0ba
Nayeong <rlaskdud10003@gmail.com>
2024-11-24 08:28:52 +0000

    .

---
+++ chap10/hw1/pp.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+strcut node{
+	int data;
+	struct node *next;
+};
+
+void push(struct node *top, int data);
+int pop(struct node *top);
+
+int main()
+{
+
+}
